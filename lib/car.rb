e84f789012384ca65986b93afce08a9e405c7e3d
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-26 10:45:50 +0300

    Done

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,99 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
+#THis is a subclass of class Vehicle
 require_relative './vehicle'
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
+
+# Car.superclass
+# # => Vehicle
+# Car.superclass.superclass
+# # => Object
+# Car.superclass.superclass.superclass
+# # => BasicObject
+# Car.superclass.superclass.superclass.superclass
+# # => nil
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
